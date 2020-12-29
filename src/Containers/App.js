503846760efd2014e3d92d15a65d209ef1a75a9b
Lea Dalibard <leadalibard@gmail.com>
2020-12-29 09:31:35 +0100

    Adding files containes, constants, components

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>My notebook</h1>
-      </header>
     </div>
   );
 }
