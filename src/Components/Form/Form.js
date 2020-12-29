1e3d0e274dcffe739dd749728d510f2c6f159220
Lea Dalibard <leadalibard@gmail.com>
2020-12-29 10:59:43 +0100

    Adding handleInputChange in form, need to add bind in the constructor if not don't recognize setState, undefined

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -15,32 +15,53 @@ class Form extends Component {
             category: '',
             date: null,
         };
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+
+    mySubmitHandler = () => {
+        alert("You are submitting " + this.state.title);
+    }
+
 
     render() {
         return (
-            <form className="form">
+            <form className="form" onSubmit={this.mySubmitHandler}>
+                <h1>Hello {this.state.title} {this.state.content}</h1>
                 <div className="form-group">
                     <label>
                         Title : </label>
                     <input className="form-control"
-                        type='text'
-                        name='title'
+                           type='text'
+                           name='title'
+                           value={this.state.title} onChange={this.handleInputChange}
                     />
                 </div>
                 <div className="form-group">
                     <label>Subject : </label>
                     <input className="form-control"
-                        type='text'
-                        name='title'
+                           type='text'
+                           name='subject'
+                           value={this.state.subject} onChange={this.handleInputChange}
                     />
                 </div>
                 <div className="form-group">
                     <label>Content : </label>
-                    <textarea className="form-control" value={this.state.content}/>
+                    <textarea className="form-control" name='content' value={this.state.content}
+                              onChange={this.handleInputChange}/>
                 </div>
 
-                <button type="button" className="submit btn-primary btn">SAVE</button>
+                <button type="submit" className="submit btn-primary btn">SAVE</button>
 
 
             </form>
@@ -49,3 +70,13 @@ class Form extends Component {
 }
 
 export default Form
+
+//  public function addPost(Post $post)
+//     {
+//         $arrayMessage=[];
+//         foreach ($this->posts as $data){
+//             array_push($arrayMessage,$data->export());
+//     }
+//         array_push($arrayMessage, $post->export());
+//         file_put_contents('messages.json', json_encode($arrayMessage, JSON_PRETTY_PRINT));
+//     }
