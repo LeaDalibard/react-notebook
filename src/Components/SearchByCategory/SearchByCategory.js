9938ed8689a342981a27311f4506f74e91cfc433
Lea Dalibard <leadalibard@gmail.com>
2021-01-05 12:30:54 +0100

    Saving updates, adding buttons to sort by categories

--- src/Components/SearchByCategory/SearchByCategory.js
+++ src/Components/SearchByCategory/SearchByCategory.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchByCategory.css'
+import Notes from "../../Constants/notes";
 
 function SearchByCategory() {
 
+    const [category, updateCategory] = useState('');
 
+    function byCategory({currentTarget}){
+        updateCategory(currentTarget.value);
+    }
+
+    console.log(Notes)
     return (
             <div className="categoriesBtn"  >
-                <button type="button" className="btn btn-info">Category 1</button>
-                <button type="button" className="btn btn-info">Category 2</button>
-                <button type="button" className="btn btn-info">Category 3</button>
-                <button type="button" className="btn btn-info">Category 4</button>
+                <button type="button" className="btn btn-info" value="Category 1" onClick={byCategory}>Category 1</button>
+                <button type="button" className="btn btn-info" value="Category 2" onClick={byCategory}>Category 2</button>
+                <button type="button" className="btn btn-info" value="Category 3" onClick={byCategory}>Category 3</button>
+                <button type="button" className="btn btn-info" value="Category 4" onClick={byCategory}>Category 4</button>
+
+               // <p>if(){Notes[0].category}</p>
             </div>
+
     )
 
 }
