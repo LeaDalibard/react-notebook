cb22ec250da5bb9bb2446c12bd125136a39bf1e7
Lea Dalibard <leadalibard@gmail.com>
2021-01-05 10:37:26 +0100

    Making dynamic search with hook 'useState

--- src/Components/SearchInput/SearchInput.js
+++ src/Components/SearchInput/SearchInput.js
@@ -1,61 +1,60 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './SearchInput.css'
 import Fuse from 'fuse.js';
 import Notes from "../../Constants/notes";
 import Note from "../../Components/Note";
 
-class SearchInput extends Component {
+function SearchInput() {
+    const [query, updateQuery] = useState('');
 
-    state = {value: ''}
-    _handleChange = ({target: {value}}) => this.setState({value})
+    const fuse = new Fuse(Notes, {
+        keys: [
+            'category',
+            'title',
+            'subject',
+            'content',
+            'date'
+        ],
+        includeScore: true
+    });
+//lowerScore => closer to an exact match
 
-    render = () => {
-        const {value} = this.state
-        const fuse = new Fuse(Notes, {
-            keys: [
-                'category',
-                'title',
-                'subject',
-                'content',
-                'date'
-            ],
-            includeScore: true
-        });
+    const results = fuse.search(query);
+    const notesResults = query ? results.map(Note => Note.item) : Notes;
 
-        //lowerScore => closer to an exact match
-
-        const results = fuse.search('lorem');
-        const notesResults = results.map(Note => Note.item)
-
-        //console.log(results);
-        //console.log(Notes);
-        console.log(notesResults)
-
-        return (
-            <div className="SearchInput">
-                <input className="query" onChange={this._handleChange} value={value}/>
-                <button type="button" className="submit">SEARCH</button>
-
-                <ul className="notesSearch">
-                    {notesResults.map(note => {
-                        const {category, title, subject, content, date, index} = note;
-
-                        return (
-                            <Note
-                                key={index}
-                                title={title}
-                                subject={subject}
-                                category={category}
-                                content={content}
-                                date={date}
-                            />
-                        )
-                    })}
-                </ul>
-            </div>
-        )
+    function onSearch({ currentTarget }) {
+        updateQuery(currentTarget.value);
     }
+
+    //console.log(results);
+    //console.log(Notes);
+    console.log(notesResults)
+
+    return (
+        <div className="SearchInput">
+            <input className="query" onChange={onSearch} value={query}/>
+            <button type="button" className="submit">SEARCH</button>
+
+            <ul className="notesSearch">
+                {notesResults.map(note => {
+                    const {category, title, subject, content, date, index} = note;
+
+                    return (
+                        <Note
+                            key={index}
+                            title={title}
+                            subject={subject}
+                            category={category}
+                            content={content}
+                            date={date}
+                        />
+                    )
+                })}
+            </ul>
+        </div>
+    )
+
 
 }
 
